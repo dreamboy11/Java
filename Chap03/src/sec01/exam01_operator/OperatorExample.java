ca6e4f4e13b6b77909fb81f40fafec46f70222df
dreamboy11 <130862727+dreamboy11@users.noreply.github.com>
2023-05-17 19:01:58 +0900

    2023.05.17

---
+++ Chap03/src/sec01/exam01_operator/OperatorExample.java
@@ -0,0 +1,17 @@
+package sec01.exam01_operator;
+
+public class OperatorExample {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 10;
+		int z = 90;
+		
+		System.out.println(x + y - z);
+		System.out.println(x - y - z);
+		System.out.println(x + y + z);
+		System.out.println(x == y);
+		System.out.println(x == z);
+
+	}
+
+}
---
+++ Chap03/src/sec02/exam01_opfirst/OpFirstExample.java
@@ -0,0 +1,20 @@
+package sec02.exam01_opfirst;
+
+public class OpFirstExample {
+	public static void main(String[] args) {
+		int var1 = 1;
+		int var2 = 3;
+		int var3 = 2;
+		int result = var1 + var2 + var3;
+		
+		System.out.println(result);
+		
+		int var4 = 10;
+		int var5 = 25;
+		int var6 = 90;
+		
+		System.out.println((var4 + var5) * var6);
+
+	}
+
+}
