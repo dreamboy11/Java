1885c429c278b6883f06f84f9176eba2e17fb135
dreamboy11 <130862727+dreamboy11@users.noreply.github.com>
2023-07-06 18:56:32 +0900

    2023.07.06

---
+++ Chap03/src/veirfy/exam05/Exercise05.java
@@ -0,0 +1,11 @@
+package veirfy.exam05;
+
+public class Exercise05 {
+	public static void main(String[] args) {
+		int value = 356;
+		
+		System.out.println(value);
+		System.out.println(356 / 100 * 100);
+	}
+
+}
